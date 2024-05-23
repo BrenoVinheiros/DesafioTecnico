35ebba0d26655f172fd7d0c3dae4db9d84bbdcef
BrenoVinheiros <brenostohler2002@gmail.com>
2024-05-23 13:55:57 -0300

    init.sql changes

--- backend/init.sql
+++ backend/init.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS iot_messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     imei VARCHAR(255) NOT NULL,
     tag VARCHAR(255) NOT NULL,
-    valor VARCHAR(255) NOT NULL,
+    valor TEXT NOT NULL,
     timestamp TIMESTAMP NOT NULL,
-    timesince INT NOT NULL
+    timesince TEXT
 );
